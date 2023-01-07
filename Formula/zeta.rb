cfb9d5fc70405b166debcaab22435696ebd4ae7b
redcode <manuel@zxe.io>
2023-01-07 18:18:41 +0100

    First commit.

---
+++ Formula/zeta.rb
@@ -0,0 +1,35 @@
+class Zeta < Formula
+  desc "Header-only general purpose library"
+  homepage "https://github.com/redcode/Zeta"
+  url "https://zeta.st/download/Zeta-0.1-pre-2022-12-04.tar.xz"
+  version "0.1-pre.2022.12.04"
+  sha256 "f5e77837ee43d65f1f9f3b3a6c0d24106a430259819d64ccebadaffa1d54f505"
+  license "LGPL-3.0-or-later"
+
+  depends_on "cmake" => :build
+
+  def install
+    system "cmake",
+      "-S", ".", "-B", "build",
+      "-DZeta_WITH_CMAKE_SUPPORT=YES",
+      "-DZeta_WITH_PKGCONFIG_SUPPORT=YES",
+      *std_cmake_args
+    system "cmake", "--install", "build"
+  end
+
+  test do
+    (testpath/"test.cpp").write <<~EOS
+      #include <Z/version.h>
+      #include <Z/macros/language.h>
+      #include <stdio.h>
+      int main(int argc, char **argv)
+        {
+        Z_UNUSED(argc) Z_UNUSED(argv)
+        puts(Z_LIBRARY_VERSION_STRING);
+        return 0;
+        }
+    EOS
+    system ENV.cc, "test.c", "-I#{include}", "-o", "test"
+    assert_equal "0.1", shell_output("./test").strip
+  end
+end
---
+++ README.md
@@ -0,0 +1,11 @@
+# Redcode Zxe
+
+## How do I install these formulae?
+
+`brew install redcode/zxe/<formula>`
+
+Or `brew tap redcode/zxe` and then `brew install <formula>`.
+
+## Documentation
+
+`brew help`, `man brew` or check [Homebrew's documentation](https://docs.brew.sh).
