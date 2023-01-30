77fb506282c13c31d6a54ee34e06c95ba9eef740
Manuel Sainz de Baranda y Goñi <manuel@zxe.io>
2023-01-31 00:18:01 +0100

    `libarchive-apple-headers` formula update.

--- Formula/libarchive-apple-headers.rb
+++ Formula/libarchive-apple-headers.rb
@@ -3,7 +3,7 @@ class LibarchiveAppleHeaders < Formula
   homepage "https://github.com/apple-oss-distributions/libarchive"
   url "https://github.com/apple-oss-distributions/libarchive/archive/refs/tags/libarchive-113.tar.gz"
   version "3.5.3" # See https://github.com/apple-oss-distributions/libarchive/blob/431afa166497a0d67ef0ede2ed04b121a3b5a9d3/libarchive.plist
-  sha256 "b422c37cc5f9ec876d927768745423ac3aae2d2a85686bc627b97e22d686930f"
+  sha256 "1a7d8d15b2e5ecba62cc5779a9f0e811ffa9221c7805b136ceb9c2ee7d8dc0d5"
   license "BSD-2-Clause"
 
   def install
