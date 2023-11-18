933364c7bfe2daca470f75c720e7ac9aafdcc297
redcode <manuel@zxe.io>
2023-11-18 21:05:11 +0100

    Add the `zxspectrum-firmware` formula.

---
+++ Formula/zxspectrum-firmware.rb
@@ -0,0 +1,18 @@
+class ZXSpectrumFirmware < Formula
+  desc "ZX Spectrum ROM images"
+  homepage "https://github.com/redcode/ZXSpectrum/wiki"
+  url "https://zxe.io/depot/ZX%20Spectrum%20-%20Firmware%20%282023-11-18%29.7z"
+  version "2023-11-18"
+  sha256 "14e0d5ffedf88e42b7b8caf173fdcd8f93e9463885df955ec257c0db94399df6"
+  license :cannot_represent
+
+  depends_on "p7zip" => :build
+
+  def install
+    (lib/"emulation/firmware").install Dir["*.rom"]
+  end
+
+  test do
+    system(true)
+  end
+end
