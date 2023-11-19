8d05141f1e55d3dfd910e89cb0bf1341fb88b3df
redcode <manuel@zxe.io>
2023-11-19 04:08:00 +0100

    `zxspectrum-firmware` formula update.

--- Formula/zxspectrum-firmware.rb
+++ Formula/zxspectrum-firmware.rb
@@ -1,7 +1,7 @@
 class ZxspectrumFirmware < Formula
   desc "ZX Spectrum ROM images"
   homepage "https://github.com/redcode/ZXSpectrum/wiki"
-  url "https://zxe.io/depot/ZX%20Spectrum%20-%20Firmware%20%282023-11-18%29.7z"
+  url "https://zxe.io/depot/download/ZX%20Spectrum%20-%20Firmware%20%282023-11-18%29.7z"
   sha256 "14e0d5ffedf88e42b7b8caf173fdcd8f93e9463885df955ec257c0db94399df6"
   license :cannot_represent
 
