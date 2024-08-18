9a59eb0c5c126af0e8ff14f953892fc8abca9c79
redcode <manuel@zxe.io>
2024-08-18 02:38:24 +0200

    `zeta` formula update.

--- Formula/zeta.rb
+++ Formula/zeta.rb
@@ -1,9 +1,9 @@
 class Zeta < Formula
   desc "Header-only general purpose library"
   homepage "https://github.com/redcode/Zeta"
-  url "https://zeta.st/download/Zeta-0.1-pre-2024-04-18.tar.xz"
-  version "0.1-pre-2024-04-18"
-  sha256 "4d535efb0c9caafe7ebcd0c8f03fa47b8e1a5c57eb6984e5363c2fed8e947e9c"
+  url "https://zeta.st/download/Zeta-0.1-pre-2024-08-18.tar.xz"
+  version "0.1-pre-2024-08-18"
+  sha256 "71b1687749dc7ff7da21e022a6db7280acdc6e38ab2ad3a9fceadf6695dd5f28"
   license "LGPL-3.0-or-later"
 
   depends_on "cmake" => :build
